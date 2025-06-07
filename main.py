17f7e790f6d86d5f062d9920596d6abf98aebdfd
Snack Bag <zarosarora@gmail.com>
2025-06-07 00:48:49 -0500

    temp: auto run on deploy

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def run_alert():
     print("Running screener...")
     top_stocks = fetch_candidates()
     if top_stocks:
+        print("Top Stocks:", top_stocks)
         send_push_notification(top_stocks)
     else:
         print("No strong trades found.")
 
-if __name__ == "__main__":
     run_alert()
